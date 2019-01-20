2e55dfc7666355ab409cdbc3b3bc70342919c0ed
Gavin Quirk <gavinquirk@gmail.com>
2019-01-20 13:24:40 -0800

    3 components, slide, fade, toggle fade

---
+++ src/components/Component3.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Component3() {
+  return (
+    <div style={c3Style}>
+      <h1>Component 3</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+         Minima iure facilis repellendus beatae a. Suscipit alias,
+          porro consequatur atque minus temporibus vel sapiente praesentium 
+          doloremque ullam deserunt error magni expedita!</p>
+    </div>
+  )
+}
+
+const c3Style = {
+  background: 'skyblue',
+  color: 'white',
+  padding: '1.5rem 1.5rem 5rem 1.5rem'
+}
